f462f722b16a3c5fedca05bf5224d6fad58f3aae
ndchung <ndchung892@gmail.com>
2023-11-21 13:34:16 +0700

    final update

---
+++ MockProjectObjectiveC/Sources/New Group/A.h
@@ -0,0 +1,12 @@
+//
+//  A.h
+//  MockProjectObjectiveC
+//
+//  Created by Chung on 25/08/2023.
+//
+
+#ifndef A_h
+#define A_h
+
+
+#endif /* A_h */
---
+++ MockProjectObjectiveC/Sources/New Group/A.m
@@ -0,0 +1,8 @@
+//
+//  A.m
+//  MockProjectObjectiveC
+//
+//  Created by Chung on 25/08/2023.
+//
+
+#import <Foundation/Foundation.h>
